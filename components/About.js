fe94f48cb74b039f5515830c0292a924288c70e4
Himanshu Singh <heman.sam@gmail.com>
2024-02-21 01:27:09 -0600

    added links to home page buttons and copy email to clipboard feature

--- components/About.js
+++ components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Intro from "./Intro";
-import SocialItem from "./SocialItem";
+import { useSnackbar } from "react-simple-snackbar";
+
 import {
   BsGithub,
   BsLinkedin,
@@ -10,6 +11,15 @@ import {
 } from "react-icons/bs";
 
 function About() {
+  const [openSnackbar, closeSnackbar] = useSnackbar({
+    position: "bottom-left",
+  });
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText("himanshu_singh@tamu.edu");
+    openSnackbar("Email copied", 5000);
+  }
+
   return (
     <div className="relative h-[90vh] text-center">
       {/* Name + tagline */}
@@ -17,16 +27,28 @@ function About() {
       {/* Social  */}
       <div className="absolute rounded-lg opacity-0 left-0 top-72 bg-[#404e75]/80 p-2 md:opacity-80">
         <div className="p-2 my-2 rounded-lg hover:bg-[#ef010f] hover:text-white">
-          <BsGithub size={40} />
+          <a href="https://github.com/HimanshuSinghGH" target="_blank">
+            <BsGithub size={40} />
+          </a>
         </div>
         <div className="p-2 my-2 rounded-lg hover:bg-[#ef010f]  hover:text-white">
-          <BsLinkedin size={40} />
+          <a
+            href="https://www.linkedin.com/in/himanshu-singh-li/"
+            target="_blank"
+          >
+            <BsLinkedin size={40} />
+          </a>
         </div>
         <div className="p-2 my-2 rounded-lg hover:bg-[#ef010f]  hover:text-white">
-          <BsFillBookFill size={40} />
+          <a
+            href="https://scholar.google.com/citations?user=r81kNvAAAAAJ&hl=en"
+            target="_blank"
+          >
+            <BsFillBookFill size={40} />
+          </a>
         </div>
         <div className="p-2 my-2 rounded-lg hover:bg-[#ef010f]  hover:text-white">
-          <BsFillEnvelopeAtFill size={40} />
+          <BsFillEnvelopeAtFill size={40} onClick={copyToClipboard} />
         </div>
       </div>
 
